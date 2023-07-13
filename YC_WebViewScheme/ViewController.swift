304a57f34a61088ef1becddab2004d872fa5442c
prettyturtle <unifconti@gmail.com>
2023-07-13 22:04:27 +0900

    Initial Commit

---
+++ YC_WebViewScheme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YC_WebViewScheme
+//
+//  Created by yc on 2023/07/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
